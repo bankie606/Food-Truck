b65338d594b6006552d5cd36c567b089e5547a4b
mikearchie <michael.archambault@seattlecentral.edu>
2017-04-25 21:36:16 -0700

    here are the forms for defining items and collecting orders
    I also renamed one of the .php files.

---
+++ item-forms.php
@@ -0,0 +1,60 @@
+<?php
+if (isset($_REQUEST['action']))
+    $myAction = trim($_REQUEST['action']);
+else
+    $myAction = "";
+
+switch ($myAction) {
+    case "display":
+        showData();
+        break;
+    default:
+        showForm();
+}
+
+//show form since no action has been completed yet
+function showForm()
+{
+    showHeader();
+
+    global $items;
+    echo '<form action="item-forms.php" method="post">';
+    foreach ($items as $item) {
+        //create a text input in the form for each item.
+        //name attribute is "item_[item->ID]"
+        echo '<p>' .$item->Name . ' <input type="text" name="item_' . $item->ID . '" /></p><p>';
+        foreach ($item->Extras as $extra) {
+            $extraCounter = 1;
+            //create a checkbox for each extra under the current item.
+            //name attribute is "extra_[item->ID]-[1,2,3,...]""
+            echo '<input type="checkbox" name="extra_' . $item->ID . '-' . $extraCounter . '"/>' . $extra . '   ';
+            $extraCounter++;
+        }
+        echo '</p>';
+    }
+
+    //create submit button and hidden input
+    echo '<p>
+            <input type="submit" value="Submit">
+         </p>
+         <input type="hidden" name="action" value="display" />
+         </form>';
+
+    showFooter();
+} //end of showForm()
+
+function showData()
+{
+    echo '<p>show data here</p>';
+}
+
+function showHeader()
+{
+    include 'header_inc.php';#defaults to header_inc.php
+}
+
+function showFooter()
+{
+    include 'footer_inc.php'; #defaults to footer_inc.php
+}
+?>
--- items.php
+++ items.php
@@ -1,5 +1,7 @@
 <?php
-//items3.php
+//foodtruck.php
+
+//include 'index.php';
 
 $myItem = new Item(1,"Taco","Our Tacos are awesome!",4.95);
 $myItem->addExtra("Sour Cream");
@@ -20,41 +22,42 @@ $myItem->addExtra("Lemon Wedges");
 $myItem->addExtra("Avacado");
 $items[] = $myItem;
 
+$myItem = new Item(2, "Pizza", "Our pizzas are yummy!", 12.99);
+$myItem->addExtra("Cheese");
+$myItem->addExtra("Sausage");
+$myItem->addExtra("Pepperoni");
+$myItem->addExtra("Olives");
+$items[] = $myItem;
 
-//create a counter to load the ids...
-//$items[] = new Item(1,"Taco","Our Tacos are awesome!",4.95);
-//$items[] = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
-//$items[] = new Item(3,"Salad","Our Salads are awesome!",5.95);
-
-echo '<pre>';
-var_dump($items);
-echo '</pre>';
-
+// echo '<pre>';
+// var_dump($items);
+// echo '</pre>';
 
 class Item
 {
     public $ID = 0;
-    public $Name = '';
-    public $Description = '';
+    public $Name = "";
+    public $Description = "";
     public $Price = 0;
     public $Extras = array();
-    
-    public function __construct($ID,$Name,$Description,$Price)
+
+    public function __construct($id, $name, $description, $price)
     {
-        $this->ID = $ID;
-        $this->Name = $Name;
-        $this->Description = $Description;
-        $this->Price = $Price;
-        
+        $this->ID = (int)$id;
+        $this->Name = $name;
+        $this->Description = $description;
+        $this->Price = (float)$price;
+
     }#end Item constructor
-    
+
     #adds extra's item and price
     public function addExtra($extra)
     {
-        $this->Extras[$extra] = 0.25;
-        
+        //$this->Extras[$extra] = 0.25;
+        $this->Extras[] = $extra;
+
     }#end addExtra()
 
 }#end Item class
 
-
+include 'item-forms.php';
